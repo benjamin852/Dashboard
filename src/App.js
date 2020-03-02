fd28d47de2f2e886bcab26040727c391a57bb497
Ben Weinberg <weinbergben33@gmail.com>
2020-03-02 17:14:12 -0500

    working hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Dashboard from "./dashboard";
 import fetchData from "./utils/fetchData";
-import "./App.css";
 
 function App() {
-  let [allBots, setAllBots] = useState([]);
-  useEffect(() => {
-    // fetchData("http://localhost:3000/threads", {}).then(result => {
-    //   console.log(result);
-    // });
-    fetch("http://localhost:3004/threads", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Success:", JSON.stringify(data));
-      });
-  }, []);
   return <Dashboard />;
 }
 
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -9,6 +9,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import MetaverseImage from "./assets/metaverse-image.jpg";
 import TextField from "@material-ui/core/TextField";
+import fetchData from "./utils/fetchData";
 
 const useStyles = makeStyles({
   root: {
@@ -23,6 +24,23 @@ focus points -> for increased ux
 */
 
 export default function Dashbaord() {
+  let [allBots, setAllBots] = useState([]);
+  useEffect(() => {
+    // fetchData("http://localhost:3000/threads", {}).then(result => {
+    //   console.log(result);
+    // });
+    fetch("http://localhost:3004/threads", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setAllBots(data);
+      });
+  }, []);
+
   const classes = useStyles();
 
   return (
