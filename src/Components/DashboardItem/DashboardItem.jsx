0c7743a12b47f249794c8c86878f8861c3cd7ef4
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-03-03 11:39:50 -0500

    buttons added based on the bot state

---
+++ src/Components/DashboardItem/DashboardItem.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import MetaverseImage from "../../assets/metaverse-image.jpg";
+import TextField from "@material-ui/core/TextField";
+// import fetchData from "../utils/fetchData";
+import EditIcon from "@material-ui/icons/Edit";
+
+import "./DashboardItem.css";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  }
+});
+
+/*
+TODO:
+dividers -> for expanding
+focus points -> for increased ux
+*/
+
+const DashboardItem = () => {
+  let [allBots, setAllBots] = useState([]);
+  let [editState, setEditState] = useState(false);
+  let [botStatus, setBotStatus] = useState(true);
+
+  useEffect(() => {
+    // fetchData("http://localhost:3000/threads", {}).then(result => {
+    //   console.log(result);
+    // });
+    fetch("http://localhost:3004/threads", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setAllBots(data);
+      });
+  }, []);
+
+  const classes = useStyles();
+
+  const editStrategy = () => setEditState(!editState);
+
+  return (
+    <Card
+      className={`${classes.root} ${botStatus ? "bot-running" : "bot-paused"}`}
+    >
+      <CardHeader
+        action={<EditIcon onClick={editStrategy} />}
+        title="Strategy Results"
+        subheader="September 14, 2019 - March 2, 2020"
+      />
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Crypto Dashboard"
+          height="140"
+          image={MetaverseImage}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <TextField
+            defaultValue="Result #1"
+            disabled={editState ? false : true}
+          />
+          <br />
+          <br />
+          <TextField
+            defaultValue="Result #2"
+            disabled={editState ? false : true}
+          />
+          <br />
+          <br />
+          <TextField
+            defaultValue="Result #3"
+            disabled={editState ? false : true}
+          />
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        {editState ? (
+          <Button color="primary">SAVE!</Button>
+        ) : botStatus ? (
+          <Button color="red">STOP</Button>
+        ) : (
+          <Button color="green">RUN</Button>
+        )}
+      </CardActions>
+    </Card>
+  );
+};
+
+export default DashboardItem;
